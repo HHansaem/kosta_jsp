388f5ad304d588acd2c68356448969d496bd9597
HHansaem <hhs01071331@gmail.com>
2024-03-06 10:18:45 +0900

    Bank (login)

--- BankProj/src/main/java/servlet/Login.java
+++ BankProj/src/main/java/servlet/Login.java
@@ -1,11 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import vo.Member;
 
 /**
  * Servlet implementation class Login
@@ -26,7 +31,25 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		
+		RequestDispatcher dispatcher = null;
+		
+		HttpSession session = request.getSession();
+		Member member = (Member) session.getAttribute(id);
+		
+		if(member == null) {  //id error
+			request.setAttribute("err", "존재하지 않는 아이디 입니다");
+			dispatcher = request.getRequestDispatcher("error.jsp");
+		} else if(member.getPassword().equals(password) == false) {  //password error
+			request.setAttribute("err", "비밀번호가 틀립니다");
+			dispatcher = request.getRequestDispatcher("error.jsp");
+		} else {  //success : session에 사용자 정보 저장
+			session.setAttribute("user", member.getId());
+			dispatcher = request.getRequestDispatcher("makeAccount.jsp");
+		}
+		dispatcher.forward(request, response);
 	}
-
 }
