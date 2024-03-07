74a8b023c5623133a6000ff9bb5f085a75c20b5f
HHansaem <hhs01071331@gmail.com>
2024-03-07 16:42:13 +0900

    Bank DB (입금)

--- BankDBProj/src/main/java/controller/Withdraw.java
+++ BankDBProj/src/main/java/controller/Withdraw.java
@@ -1,4 +1,4 @@
-package servlet;
+package controller;
 
 import java.io.IOException;
 
@@ -8,9 +8,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import vo.Account;
+import dto.Account;
+import service.AccountService;
+import service.AccountServiceImpl;
 
 /**
  * Servlet implementation class Withdraw
@@ -28,30 +29,34 @@ public class Withdraw extends HttpServlet {
     }
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-		String id = request.getParameter("id");
-		int money = Integer.parseInt(request.getParameter("money"));
-		
-		HttpSession session = request.getSession();
-		Account acc = (Account)session.getAttribute(id);
-		RequestDispatcher dispatcher = null;
-		
-		if(acc == null) {
-			request.setAttribute("err", "계좌번호가 틀립니다");
-			dispatcher = request.getRequestDispatcher("error.jsp");
-		} else {
-			acc.withdraw(money);
-			request.setAttribute("acc", acc);
-			dispatcher = request.getRequestDispatcher("accountInfo.jsp");
-		}
-		dispatcher.forward(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("withdraw.jsp").forward(request, response);
 	}
 	
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	      req.getRequestDispatcher("withdraw.jsp").forward(req, resp);
-	   }
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//1. request 인코딩 지정
+		request.setCharacterEncoding("utf-8");
+		//2. 파라미터 가져오기
+		String id = request.getParameter("id");
+		int money = Integer.parseInt(request.getParameter("money"));
+		RequestDispatcher dispatcher = null;
+		try {
+			//3. 서비스의 출금 호출하기 (예외처리)
+			AccountService accountService = new AccountServiceImpl();
+			accountService.withdraw(id, money);
+			Account acc = accountService.accountInfo(id);
+			request.setAttribute("acc", acc);
+			dispatcher = request.getRequestDispatcher("accountInfo.jsp");
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("err", e.getMessage());
+			dispatcher = request.getRequestDispatcher("error.jsp");
+		}
+		//4. 계좌조회 포워딩하기
+		dispatcher.forward(request, response);
+	}
 
 }
