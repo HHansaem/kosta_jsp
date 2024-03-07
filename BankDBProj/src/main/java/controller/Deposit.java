2501b42750f81d98208d5bcc4f72da7683b4263a
HHansaem <hhs01071331@gmail.com>
2024-03-07 16:43:22 +0900

    Bank DB (입금)

--- BankDBProj/src/main/java/controller/Deposit.java
+++ BankDBProj/src/main/java/controller/Deposit.java
@@ -1,4 +1,4 @@
-package servlet;
+package controller;
 
 import java.io.IOException;
 
@@ -8,12 +8,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import vo.Account;
+import dto.Account;
+import service.AccountService;
+import service.AccountServiceImpl;
 
 /**
- * Servlet implementation class deposit
+ * Servlet implementation class Deposit
  */
 @WebServlet("/deposit")
 public class Deposit extends HttpServlet {
@@ -28,30 +29,31 @@ public class Deposit extends HttpServlet {
     }
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("deposit.jsp").forward(request, response);
+	}
+	
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		String id = request.getParameter("id");
 		int money = Integer.parseInt(request.getParameter("money"));
-		
-		HttpSession session = request.getSession();
-		Account acc = (Account)session.getAttribute(id);  //id데이터 공유하기 위해 세션에 담기
 		RequestDispatcher dispatcher = null;
 		
-		if(acc == null) {  //acc(id세션)없으면 dispatcher를 에러페이지로 설정
-			request.setAttribute("err", "계좌번호가 틀립니다");
-			dispatcher = request.getRequestDispatcher("error.jsp");
-		} else {
-			acc.deposit(money);
+		try {
+			AccountService accountService = new AccountServiceImpl();
+			accountService.deposit(id, money);
+			Account acc = accountService.accountInfo(id);
 			request.setAttribute("acc", acc);
 			dispatcher = request.getRequestDispatcher("accountInfo.jsp");
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("err", e.getMessage());
+			dispatcher = request.getRequestDispatcher("error.jsp");
 		}
-		dispatcher.forward(request, response);  //설정된 dispatcher 페이지로 request 데이터 보내주기
+		dispatcher.forward(request, response);
 	}
-	
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	      req.getRequestDispatcher("deposit.jsp").forward(req, resp);
-	   }
 
 }
