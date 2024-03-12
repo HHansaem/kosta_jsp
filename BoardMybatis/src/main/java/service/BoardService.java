4600fc197af11abaf875e21018a72c655a072e72
HHansaem <hhs01071331@gmail.com>
2024-03-12 18:06:28 +0900

    Bank Mybatis (Service 수정)

--- BoardMybatis/src/main/java/service/BoardService.java
+++ BoardMybatis/src/main/java/service/BoardService.java
@@ -1,7 +1,12 @@
 package service;
 
+import javax.servlet.http.HttpServletRequest;
+
 import dto.Board;
 
 public interface BoardService {
-	void boardWrite(Board board) throws Exception;
+	void boardWrite(HttpServletRequest request) throws Exception;
+	void boardListByPage(HttpServletRequest request) throws Exception;
+	Board boardDetail(Integer num) throws Exception;
+	void boardModify(HttpServletRequest request) throws Exception;
 }
