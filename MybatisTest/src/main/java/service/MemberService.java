9ff2f26091bd06ac766c76c1055fa67fe08a181a
HHansaem <hhs01071331@gmail.com>
2024-03-14 14:16:02 +0900

    MybatisTest (member service)

--- MybatisTest/src/main/java/service/MemberService.java
+++ MybatisTest/src/main/java/service/MemberService.java
@@ -2,8 +2,8 @@ package service;
 
 import javax.servlet.http.HttpServletRequest;
 
-import dto.Member;
-
 public interface MemberService {
 	void join(HttpServletRequest request) throws Exception;
+	void login(HttpServletRequest request) throws Exception;
+	boolean memberIdCheck(String id) throws Exception;
 }
--- MybatisTest/src/main/java/service/MemberServiceImpl.java
+++ MybatisTest/src/main/java/service/MemberServiceImpl.java
@@ -1,6 +1,7 @@
 package service;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import dao.MemberDAO;
 import dao.MemberDAOImple;
@@ -11,14 +12,41 @@ public class MemberServiceImpl implements MemberService {
 	
 	@Override
 	public void join(HttpServletRequest request) throws Exception {
-		Member member = new Member();
-		member.setId(request.getParameter("id"));
-		member.setName(request.getParameter("name"));
-		member.setPassword(request.getParameter("password"));
-		member.setEmail(request.getParameter("email"));
-		member.setAddress(request.getParameter("address"));
+		request.setCharacterEncoding("utf-8");
 		
+		String id = request.getParameter("id");
+		Member smember = memberDAO.selectMember(id);
+		if(smember != null) throw new Exception("아이디가 중복됩니다");
+		
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		String address = request.getParameter("address");
+		
+		Member member = new Member(id, name, password, email, address);
 		memberDAO.insertMember(member);
+	}
+
+	@Override
+	public void login(HttpServletRequest request) throws Exception {
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		Member member = memberDAO.selectMember(id);
+		if(member == null) throw new Exception("아이디가 존재하지 않습니다");
+		if(!member.getPassword().equals(password)) throw new Exception("비밀번호가 틀립니다");
+		
+		//로그인 정상
+		HttpSession session = request.getSession();
+		member.setPassword("");  //비밀번호는 빼고 세션에 저장 (비번은 중요하니까..)
+		session.setAttribute("user", member);
+		
+	}
+
+	@Override
+	public boolean memberIdCheck(String id) throws Exception {
+		Member member = memberDAO.selectMember(id);
+		if(member == null) return false;
+		return true;
 	}
 
 }
